47a15eb47038cc46c900c1baa1e466f88961d719
ankitkumar5049 <72911975+ankitkumar5049@users.noreply.github.com>
2022-11-30 19:51:29 +0530

    meaage

--- app/src/main/java/com/example/anew/MainActivity.java
+++ app/src/main/java/com/example/anew/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -17,34 +19,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         navigation = findViewById(R.id.navigation_button);
         search = findViewById(R.id.edit_search);
         imageButton = findViewById(R.id.imageButton);
-
-        navigation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),
-                        "Navigation Bar",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),
-                        "Navigation Bar",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),
-                        "Navigation Bar",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
