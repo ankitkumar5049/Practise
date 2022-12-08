d18629cc6ff2efa29a4ca811ca202c7e17cc2a54
ankitkumar5049 <72911975+ankitkumar5049@users.noreply.github.com>
2022-12-08 23:24:46 +0530

    nothing

--- app/src/main/java/com/example/anew/Activity/SecondActivity.java
+++ app/src/main/java/com/example/anew/Activity/SecondActivity.java
@@ -1,19 +1,47 @@
 package com.example.anew.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.example.anew.R;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
+        bottomNavigationView.setOnItemReselectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.home :
+                    break;
+
+                case R.id.setting:
+                    break;
+
+                case R.id.profile:
+                    break;
+
+                case R.id.contact:
+                    break;
+
+            }
+            return;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.replace(R.id.)
     }
 
     @Override
